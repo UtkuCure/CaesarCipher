8b3e22d7f15d8775013dca6d03bebd7b615fff59
Utku Cüre <cureutku@gmail.com>
2026-04-23 20:35:31 +0300

    Add UI chat

--- Program.cs
+++ Program.cs
@@ -6,6 +6,38 @@ internal class Program
 {
     public static void Main(string[] args)
     {
+        int currentLettersAlphabetIndex;
+        int AlphabetLength = turkishAlphabet.Count;
+        int AlphabetLastIndex = turkishAlphabet.Count - 1;
+        
+        
+        GetTurkishAlphabetFromEnvironment();
+        
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("Enter a text you want to encrypt: ");
+        Console.ResetColor();
+        
+        string inputText = Console.ReadLine();
+        
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("Enter the number of scroll (-3, +3): ");
+        int scrollNumber = int.Parse(Console.ReadLine());
+
+        List<char> outputText = inputText.ToLower().ToCharArray().ToList();
+        
+        for (int i = 0; i < outputText.Count; i++)
+        {
+            
+        }
+        
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        outputText.ToString();
+        Console.Write(outputText);
+        Console.ResetColor();
+    }
+
+    private static void GetTurkishAlphabetFromEnvironment()
+    {
         Env.Load();
         List<char> turkishAlphabet = new List<char>();
             
