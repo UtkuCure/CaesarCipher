0433d8bf5a7d388a635e91386a61d6d53fd362a6
Utku Cüre <cureutku@gmail.com>
2026-04-23 11:10:18 +0300

    StartDebugging

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
-﻿namespace CaesarCipher;
+﻿
+namespace CaesarCipher;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
+        //Code has bugs
         List<char> turkishAlphabet;
         
         string rawAlphabet = Environment.GetEnvironmentVariable("TurkishAlphabet");
         turkishAlphabet = rawAlphabet.ToCharArray().ToList();
         
         //Test
-        
+        Console.WriteLine(turkishAlphabet.Count);
+        Console.WriteLine(turkishAlphabet[0]);
+        Console.WriteLine(turkishAlphabet[1]);
+        int lastIndex =  turkishAlphabet.Count - 1;
+        Console.WriteLine(lastIndex);
+        Console.WriteLine(turkishAlphabet[lastIndex]);
     }
 }
