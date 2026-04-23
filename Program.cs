10e675e8d2b01b8800cf3c1bb4abf3674de45c3b
Utku Cüre <cureutku@gmail.com>
2026-04-23 10:47:06 +0300

    Fix: static main issue

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 internal class Program
 {
-    public void Main(string[] args)
+    public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("This is a test for SSH key!");
