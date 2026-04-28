260aa3097cd53e8002ef64ed2b276fa791293607
Utku Cüre <cureutku@gmail.com>
2026-04-28 22:06:06 +0300

    feat: add english alphabet and alphabet selection
    users wasn't have english alphabet and that was a problem
    there must be an alphabet selection for different needs
    also add a loop for better user experience and fastes test

--- Program.cs
+++ Program.cs
@@ -37,43 +37,60 @@ public class Program
     
     public static void Main(string[] args)
     {
-        //GetTurkishAlphabetFromEnvironment();
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("Wich alphabet do you want to use (english/turkish) (e/t)?");
+        Console.ResetColor();
+
+        string? alphabetType = Console.ReadLine()?.ToLower();
+
+        if (alphabetType is null) return;
+
+        if (alphabetType == "t" || alphabetType == "turkish")
+        {
+            _rawAlphabet = "abcçdefgğhıijklmnoöprsştuüvyz";
+        }
+        else if (alphabetType == "e" || alphabetType == "english")
+        {
+            _rawAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        }
         
-        _rawAlphabet = "abcçdefgğhıijklmnoöprsştuüvyz";
         _turkishAlphabet = _rawAlphabet.ToCharArray().ToList();
         
         int alphabetLength = _turkishAlphabet.Count;
-        
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.Write("Enter a text you want to encrypt: ");
-        Console.ResetColor();
-        
-        string? inputText = Console.ReadLine();
-        if (inputText is null) 
-        {
-            Console.WriteLine("Input cannot be null, exiting...");
-            return;
-        }
-        
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.Write("Enter the number of scroll (-3, +3): ");
-        Console.ResetColor();
-        
-        string? inputScrollNumber = Console.ReadLine();
-        if(int.TryParse(inputScrollNumber, out int scrollNumber) == false)
-        {
-            Console.WriteLine("Invalid scroll number, exiting...");
-            return;
-        }
 
-        List<char> outputText = inputText.ToLower(new CultureInfo("tr-TR")).ToCharArray().ToList();
-
-        EncryptWithCaesarCipher(outputText, scrollNumber, alphabetLength);
+        while (true)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Enter a text you want to encrypt: ");
+            Console.ResetColor();
         
-        Console.ForegroundColor = ConsoleColor.DarkYellow;
-        string finalOutput = new string(outputText.ToArray());
-        Console.Write(finalOutput);
-        Console.ResetColor();
+            string? inputText = Console.ReadLine();
+            if (inputText is null) 
+            {
+                Console.WriteLine("Input cannot be null, exiting...");
+                return;
+            }
+        
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Enter the number of scroll (-3, +3): ");
+            Console.ResetColor();
+        
+            string? inputScrollNumber = Console.ReadLine();
+            if(int.TryParse(inputScrollNumber, out int scrollNumber) == false)
+            {
+                Console.WriteLine("Invalid scroll number, exiting...");
+                return;
+            }
+
+            List<char> outputText = inputText.ToLower(new CultureInfo("tr-TR")).ToCharArray().ToList();
+
+            EncryptWithCaesarCipher(outputText, scrollNumber, alphabetLength);
+        
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            string finalOutput = new string(outputText.ToArray());
+            Console.Write(finalOutput);
+            Console.ResetColor();
+        }
     }
 
     public static void EncryptWithCaesarCipher(List<char> outputText, int scrollNumber, int alphabetLength)
